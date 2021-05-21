c6636657da7ce0d87a7906b1e79b0d8c650e9f79
cbreckenridge <cbreckenridge@ucmerced.edu>
2021-05-21 11:56:45 -0700

    first outline of solver file

---
+++ solver.py
@@ -0,0 +1,29 @@
+# Main file for getting satisfiable assignment of bin packing instance
+# Christine Breckenridge
+
+from pysmt.shortcuts import Symbol, And, Or, Solver, Iff, Implies
+
+# Set 1: All objects are packed
+# TODO
+
+# Set 2: Consecutive linear ordering
+# TODO
+
+# Set 3: No-overlap constraint
+# TODO
+
+# Set 4: Stable set feasibility
+# TODO
+# Must generate all infeasible sets
+
+# Set 5: No empy cliques
+# TODO
+
+# Set 6: Correlations between vars
+# TODO
+
+# Combine formulas
+# TODO
+
+# Get sat assignment if possible
+# TODO
